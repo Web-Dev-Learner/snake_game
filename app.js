908531c86d390058de7e38acb23e52d26a640485
Web-Dev-Learner <shribabe2@gmail.com>
2024-06-12 21:01:24 +0530

    food

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const gameboard = document.getElementById('gameboard');
 const context = gameboard.getContext('2d');
 
-
+const scoreText = document.getElementById('scoreval');
 
 
 const WIDTH = gameboard.width;
@@ -12,7 +12,10 @@ let foodX;
 let foodY;
 let xVel = 25;
 let yVel = 0;
-
+let score = 0;
+let active=true;
+let started = false;
+let paused = false;
 
 
 let snake = [
@@ -22,7 +25,7 @@ let snake = [
     {x:0,y:0}
 ];
 
-
+window.addEventListener('keydown',keyPress);
 startGame();
 
 function startGame(){
@@ -36,6 +39,22 @@ function startGame(){
     
 }
 
+function clearBoard(){
+    context.fillStyle = '#212121';
+    //fillRect(xStart,yStart,width,height)
+    context.fillRect(0,0,WIDTH,HEIGHT);
+}
+
+function createFood(){
+    foodX = Math.floor(Math.random()*WIDTH/UNIT)*UNIT;
+    foodY = Math.floor(Math.random()*HEIGHT/UNIT)*UNIT;
+}
+
+function displayFood(){
+    context.fillStyle = 'red';
+    context.fillRect(foodX,foodY,UNIT,UNIT)
+}
+
 function drawSnake(){
     context.fillStyle = 'aqua';
     context.strokeStyle = '#212121';
@@ -45,11 +64,92 @@ function drawSnake(){
     })
 }
 
-function createFood(){
-    foodX = Math.floor(Math.random()*WIDTH/UNIT)*UNIT;
-    foodY = Math.floor(Math.random()*HEIGHT/UNIT)*UNIT;
+function moveSnake(){
+    const head = {x:snake[0].x+xVel,
+                    y:snake[0].y+yVel}
+    snake.unshift(head)
+    if(snake[0].x==foodX && snake[0].y==foodY){
+        score += 1;
+        scoreText.textContent = score;
+        createFood();
+    }
+    else
+        snake.pop();
 }
-function displayFood(){
-    context.fillStyle = 'red';
-    context.fillRect(foodX,foodY,UNIT,UNIT)
+
+function nextTick(){
+    if(active && !paused){
+        setTimeout(() => {
+            clearBoard();
+            displayFood();
+            moveSnake();
+            drawSnake();
+            checkGameOver();
+            nextTick();
+        }, 100);
+    }
+    else if(!active){
+        clearBoard();
+        context.font = "bold 50px serif";
+        context.fillStyle = "white";
+        context.textAlign = "center";
+        context.fillText("Game Over!!",WIDTH/2,HEIGHT/2)
+    }
+}
+
+function keyPress(event){
+    if(!started){
+        started = true;
+        nextTick();
+    }
+    //pause when space is pressed
+    if(event.keyCode===32){
+        console.log('clicked')
+        if(paused){
+            paused = false;
+            nextTick();
+        }
+        else{
+            paused = true;
+        }
+    }
+    const LEFT = 37
+    const UP = 38
+    const RIGHT = 39
+    const DOWN = 40
+
+    switch(true){
+        //left key pressed and not going right
+        case(event.keyCode==LEFT  && xVel!=UNIT):
+            xVel=-UNIT;
+            yVel = 0;
+            break;
+        //right key pressed and not going left
+        case(event.keyCode==RIGHT && xVel!=-UNIT):
+            xVel=UNIT;
+            yVel=0;
+            break;
+        //Up key pressed and not going down
+        case(event.keyCode==UP && yVel!=UNIT):
+            xVel=0;
+            yVel=-UNIT;
+            break;
+        //down key pressed and not going up
+        case(event.keyCode==DOWN && yVel!=-UNIT):
+            xVel=0;
+            yVel=UNIT;
+            break;
+
+    }
+}
+
+function checkGameOver(){
+    switch(true){
+        case(snake[0].x<0):
+        case(snake[0].x>=WIDTH):
+        case(snake[0].y<0):
+        case(snake[0].y>=HEIGHT):
+            active=false;
+            break;
+    }
 }
